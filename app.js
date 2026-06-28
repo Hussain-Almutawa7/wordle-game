22951f7679f30c9b6e628fbd88842afc99e1dc98
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-06-28 14:24:56 +0300

    Added base js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// console.log("Hello World")
+const secretWords = [
+  "apple",
+  "grape",
+  "chair",
+  "table",
+  "plant",
+  "water",
+  "bread",
+  "house",
+  "light",
+  "sound",
+  "music",
+  "green",
+  "black",
+  "white",
+  "brown",
+  "smile",
+  "dream",
+  "cloud",
+  "stone",
+  "river",
+  "beach",
+  "heart",
+  "world",
+  "night",
+  "smart",
+  "happy",
+  "sweet",
+  "tiger",
+  "zebra",
+  "train"
+];
+
+let userGuess;
+let wordCompare;
+let numAttempts = 6;
+
+const keyRow = document.querySelectorAll(".keyboard-row");
+const board = document.querySelector(".board-guess");
+const message = document.querySelector(".message");
+
