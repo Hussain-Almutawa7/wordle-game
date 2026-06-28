1f365ecbd765f33d46f5cb79b758d000640000b2
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-06-28 16:17:38 +0300

    Add clicked letters to the board

--- app.js
+++ app.js
@@ -31,11 +31,31 @@ const secretWords = [
   "train"
 ];
 
-let userGuess;
-let wordCompare;
+let userGuess = "";
+let wordCompare = "";
 let numAttempts = 6;
+let currentBoxIndex = 0;
 
 const keyRow = document.querySelectorAll(".keyboard-row");
 const board = document.querySelector(".board-guess");
+const boxes = document.querySelectorAll(".sqr");
 const message = document.querySelector(".message");
 
+keyRow.forEach(row => {
+    const buttons = row.querySelectorAll(".keyboard-key");
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            if(btn.id === "del") {
+                return // Logic delete will be here
+            }
+
+            if(btn.id === "enter") {
+                return // Logic insert will be here
+            }
+            userGuess += btn.textContent
+            boxes[currentBoxIndex].textContent = btn.textContent
+            currentBoxIndex++;
+        });
+    });
+});
